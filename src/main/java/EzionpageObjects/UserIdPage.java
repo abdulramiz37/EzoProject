9e4648f5882d9bef9344cd467ae14e8ca90b8f6e
abdul <abdulramiz007@gmail.com>
2025-07-29 21:33:24 +0530

    first commit

--- src/main/java/EzionpageObjects/UserIdPage.java
+++ src/main/java/EzionpageObjects/UserIdPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import EzionAbstractionComponents.Abstraction;
-import PharmacyFlowObjects.RolesManagementPage;
 
 
 
