f2dda685ba00e9a5a32af27c640e8f8e2722d14e
abdul <abdulramiz007@gmail.com>
2025-08-01 12:44:32 +0530

    djndkj;

--- src/main/java/ezion/Resources/ExtentReportNg.java
+++ src/main/java/ezion/Resources/ExtentReportNg.java
@@ -1,7 +1,5 @@
 package ezion.Resources;
 
-import java.io.File;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
@@ -9,17 +7,7 @@ public class ExtentReportNg {
 
 	public static ExtentReports getReportObject()
 	{
-        String reportsDir = System.getProperty("user.dir") + File.separator + "reports";
-        File reportDir = new File(reportsDir);
-        
-        // Create the reports folder if it doesn't exist
-        if (!reportDir.exists()) {
-            reportDir.mkdirs();
-            System.out.println("Created reports directory at: " + reportsDir);
-        }
-
-        String path = reportsDir + File.separator + "index.html";
-        System.out.println("Writing report to: " + path);
+		String path =System.getProperty("user.dir")+"//reports//index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 		reporter.config().setTimelineEnabled(true);
 
@@ -28,7 +16,7 @@ public class ExtentReportNg {
 		reporter.config().setOfflineMode(true);
 		reporter.config().setReportName("Ezion Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
-		reporter.config().setTimeStampFormat("dd/MM/yyyy HH:mm:ss"); // ✅ Fix time 
+		reporter.config().setTimeStampFormat("dd/MM/yyyy HH:mm:ss"); // ✅ Fix time display
 
 		
 		ExtentReports extent =new ExtentReports();
