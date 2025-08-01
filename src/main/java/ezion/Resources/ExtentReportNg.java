f4c66f166abb087fe9b514056dc9040201e0147f
abdul <abdulramiz007@gmail.com>
2025-08-01 10:56:38 +0530

    Your commit message

--- src/main/java/ezion/Resources/ExtentReportNg.java
+++ src/main/java/ezion/Resources/ExtentReportNg.java
@@ -9,12 +9,17 @@ public class ExtentReportNg {
 	{
 		String path =System.getProperty("user.dir")+"//reports//index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setTimelineEnabled(true);
+
+		// Disable CDN loading
+		reporter.config().setEncoding("utf-8");
+		reporter.config().setOfflineMode(true);
 		reporter.config().setReportName("Ezion Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
 		
 		ExtentReports extent =new ExtentReports();
 		extent.attachReporter(reporter);
-		extent.setSystemInfo("Tester"," Abdul RamizRahul" );
+		extent.setSystemInfo("Tester"," Abdul Ramiz" );
 		return extent;
 		
 		
