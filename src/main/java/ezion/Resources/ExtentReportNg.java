9b561983dd54fe53e29183fa3d3136b34dc35c1e
abdul <abdulramiz007@gmail.com>
2025-08-01 12:08:07 +0530

    djndkj;

--- src/main/java/ezion/Resources/ExtentReportNg.java
+++ src/main/java/ezion/Resources/ExtentReportNg.java
@@ -9,8 +9,17 @@ public class ExtentReportNg {
 
 	public static ExtentReports getReportObject()
 	{
-	    String path = System.getProperty("user.dir") + File.separator + "reports" + File.separator + "index.html";
+        String reportsDir = System.getProperty("user.dir") + File.separator + "reports";
+        File reportDir = new File(reportsDir);
+        
+        // Create the reports folder if it doesn't exist
+        if (!reportDir.exists()) {
+            reportDir.mkdirs();
+            System.out.println("Created reports directory at: " + reportsDir);
+        }
 
+        String path = reportsDir + File.separator + "index.html";
+        System.out.println("Writing report to: " + path);
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 		reporter.config().setTimelineEnabled(true);
 
